7f6befe516e5447c9535c8d78734c9b74bafc12f
Joseph "DisTurBinG" Cooper <Disturbing@users.noreply.github.com>
2017-12-20 12:06:33 +0800

    :bug: bug(cache-processor): Removing unsupported api json

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -12,13 +12,7 @@ const { email, password, name, picture, role } = schema.tree
 /**
  * @api {get} /users Retrieve users
  * @apiName RetrieveUsers
- * @apiGroup User
- * @apiPermission admin
  * @apiParam {String} access_token User access_token.
- * @apiUse listParams
- * @apiSuccess {Object[]} users List of users.
- * @apiError {Object} 400 Some parameters may contain invalid values.
- * @apiError 401 Admin access only.
  */
 router.get('/',
   token({ required: true, roles: ['admin'] }),
@@ -28,10 +22,7 @@ router.get('/',
 /**
  * @api {get} /users/me Retrieve current user
  * @apiName RetrieveCurrentUser
- * @apiGroup User
- * @apiPermission user
  * @apiParam {String} access_token User access_token.
- * @apiSuccess {Object} user User's data.
  */
 router.get('/me',
   token({ required: true }),
@@ -40,10 +31,6 @@ router.get('/me',
 /**
  * @api {get} /users/:id Retrieve user
  * @apiName RetrieveUser
- * @apiGroup User
- * @apiPermission public
- * @apiSuccess {Object} user User's data.
- * @apiError 404 User not found.
  */
 router.get('/:id',
   show)
@@ -51,18 +38,12 @@ router.get('/:id',
 /**
  * @api {post} /users Create user
  * @apiName CreateUser
- * @apiGroup User
- * @apiPermission master
  * @apiParam {String} access_token Master access_token.
  * @apiParam {String} email User's email.
  * @apiParam {String{6..}} password User's password.
  * @apiParam {String} [name] User's name.
  * @apiParam {String} [picture] User's picture.
  * @apiParam {String=user,admin} [role=user] User's role.
- * @apiSuccess (Sucess 201) {Object} user User's data.
- * @apiError {Object} 400 Some parameters may contain invalid values.
- * @apiError 401 Master access only.
- * @apiError 409 Email already registered.
  */
 router.post('/',
   master(),
@@ -72,15 +53,10 @@ router.post('/',
 /**
  * @api {put} /users/:id Update user
  * @apiName UpdateUser
- * @apiGroup User
- * @apiPermission user
  * @apiParam {String} access_token User access_token.
  * @apiParam {String} [name] User's name.
  * @apiParam {String} [picture] User's picture.
  * @apiSuccess {Object} user User's data.
- * @apiError {Object} 400 Some parameters may contain invalid values.
- * @apiError 401 Current user or admin access only.
- * @apiError 404 User not found.
  */
 router.put('/:id',
   token({ required: true }),
@@ -90,13 +66,8 @@ router.put('/:id',
 /**
  * @api {put} /users/:id/password Update password
  * @apiName UpdatePassword
- * @apiGroup User
- * @apiHeader {String} Authorization Basic authorization with email and password.
  * @apiParam {String{6..}} password User's new password.
  * @apiSuccess (Success 201) {Object} user User's data.
- * @apiError {Object} 400 Some parameters may contain invalid values.
- * @apiError 401 Current user access only.
- * @apiError 404 User not found.
  */
 router.put('/:id/password',
   passwordAuth(),
@@ -106,12 +77,8 @@ router.put('/:id/password',
 /**
  * @api {delete} /users/:id Delete user
  * @apiName DeleteUser
- * @apiGroup User
- * @apiPermission admin
  * @apiParam {String} access_token User access_token.
  * @apiSuccess (Success 204) 204 No Content.
- * @apiError 401 Admin access only.
- * @apiError 404 User not found.
  */
 router.delete('/:id',
   token({ required: true, roles: ['admin'] }),
